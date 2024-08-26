843d3a94c98e9618dad1a2ad70e93db2f39b2cc6
zls <1216486490@qq.com>
2024-08-26 21:25:53 +0800

    Initial commit

---
+++ src/main/java/com/mo/moapibackend/MoapiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mo.moapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoapiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoapiBackendApplication.class, args);
+    }
+
+}
